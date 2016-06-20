f307eb42e721d6145a1b1d7fd906d4ff6568d7e0
Rasmus Bruckner <rasmusbruckner@gmail.com>
2016-06-20 20:20:19 +0200

    cleaned "Aim.m" script

--- Task/Functions/Aim.m
+++ Task/Functions/Aim.m
@@ -1,11 +1,16 @@
 function Aim(taskParam, parameter)
+%AIM   Prints the aim (as a needle) of the cannon
 
-xPredS = ((taskParam.circle.rotationRad-5) * sin(parameter*taskParam.circle.unit));
-yPredS = ((taskParam.circle.rotationRad-5) * (-cos(parameter*taskParam.circle.unit)));
-OutcSpot = OffsetRect(taskParam.circle.outcCentSpotRect, xPredS, yPredS);
-x = (OutcSpot(3)/2) + (OutcSpot(1)/2);
-y = (OutcSpot(4)/2) + (OutcSpot(2)/2);
-Screen('DrawLine', taskParam.gParam.window.onScreen, [0 0 0], taskParam.gParam.zero(1), taskParam.gParam.zero(2), x, y, 2);
+xPredS = ((taskParam.circle.rotationRad-5) *...
+    sin(parameter*taskParam.circle.unit));
+yPredS = ((taskParam.circle.rotationRad-5) *...
+    (-cos(parameter*taskParam.circle.unit)));
+outcomeCenter = OffsetRect(taskParam.circle.outcCentSpotRect, xPredS,...
+    yPredS);
+x = (outcomeCenter(3)/2) + (outcomeCenter(1)/2);
+y = (outcomeCenter(4)/2) + (outcomeCenter(2)/2);
+Screen('DrawLine', taskParam.gParam.window.onScreen, [0 0 0],...
+    taskParam.gParam.zero(1), taskParam.gParam.zero(2), x, y, 2);
 
 end
 
