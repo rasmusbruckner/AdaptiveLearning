a8ad3264b1380b12e6bf6ce5c57c80bb1c0b95c3
Rasmus Bruckner <rasmusbruckner@gmail.com>
2016-06-20 20:37:13 +0200

    cleaned "Diff.m" function

--- Task/Functions/Diff.m
+++ Task/Functions/Diff.m
@@ -1,12 +1,8 @@
 function diff = Diff(parameter1, parameter2)
-%   Depending on the input you calculate prediction error, memory error
-%   or update.
-    % this function uses dirc_dist which computes the difference between two
-    % value in radians. We therefore convert degrees to radians and recompute
-    % degrees after the calculation.
+%DIFF   Calculates the difference of two variables on a circle
+%   DIFF is used to compute prediction error, memory error
+%   or update in the cannon task
 
-
-%diff = abs(rad2deg(circ_dist(deg2rad(parameter1), deg2rad(parameter2))));
 diff = rad2deg(circ_dist(deg2rad(parameter1), deg2rad(parameter2)));
 
 end
