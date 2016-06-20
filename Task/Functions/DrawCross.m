24b312b3fdb2666d0a073e4fbd138b2b669f1f85
Rasmus Bruckner <rasmusbruckner@gmail.com>
2016-06-20 20:39:10 +0200

    cleaned "FixationCross.m" function

--- Task/Functions/DrawCross.m
+++ Task/Functions/DrawCross.m
@@ -1,8 +1,10 @@
 function DrawCross(taskParam)
-% This function draws the fixation cross.
+%DRAWCROSS   Draws the fixation cross
 
 zero = taskParam.gParam.zero;
-Screen('DrawLine', taskParam.gParam.window.onScreen, [0 0 0], zero(1) - 9, zero(2), zero(1) + 9 , zero(2), 3);
-Screen('DrawLine', taskParam.gParam.window.onScreen, [0 0 0], zero(1), zero(2) - 9, zero(1) , zero(2) + 9, 3);
+Screen('DrawLine', taskParam.gParam.window.onScreen,...
+    [0 0 0], zero(1) - 9, zero(2), zero(1) + 9 , zero(2), 3);
+Screen('DrawLine', taskParam.gParam.window.onScreen,...
+    [0 0 0], zero(1), zero(2) - 9, zero(1) , zero(2) + 9, 3);
 end
 
