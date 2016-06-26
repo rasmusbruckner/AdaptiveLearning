632448c6d03f09e7dcb935beb229093ed4226364
Rasmus Bruckner <rasmusbruckner@gmail.com>
2016-06-26 15:17:49 +0200

    cleaned "identifyPC.m"

--- Task/Functions/identifyPC.m
+++ Task/Functions/identifyPC.m
@@ -1,6 +1,6 @@
 function [computer, Computer2] = identifyPC()
-% This function identifies the computer the task is running on by checking
-% the software identifier of its network card.
+%IDENTIFYPC   Identifies the computer the task is running on by checking
+%the software identifier of its network card.
 
 sid = '';
 ni = java.net.NetworkInterface.getNetworkInterfaces;
@@ -20,9 +20,11 @@ if strcmp(sid, '.EDB04B8B5134')
 elseif strcmp(sid, '.EDB0518B54C7')
     computer = 'D_Pilot';
     Computer2 = true;
-elseif strcmp(sid, '.139DEA01CE83.8888888600000000.8888888600000000.8888888600000000') %strcmp(sid, '.139DEA01CE83.8888888600000000.8888888600000000.8888888600000000')
+elseif strcmp(sid, ['.139DEA01CE83.8888888600000000.8888888600000000.'...
+        '8888888600000000'])
     computer = 'Dresden';
-elseif strcmp(sid, '.F5B0D5445F09.8888888600000000.88A8B78070C3.8888888600000000.8888888600000000')
+elseif strcmp(sid, ['.F5B0D5445F09.8888888600000000.88A8B78070C3.'...
+        '8888888600000000.8888888600000000'])
     computer = 'Dresden_Rene';
 elseif strcmp(sid, '.0928C78F12F8')
     computer = 'Macbook';
