536e9c457399af7ac0e6379515e29eceb2772934
Rasmus Bruckner <rasmusbruckner@gmail.com>
2016-06-26 15:28:09 +0200

    cleaned "TickMark.m"

--- Task/Functions/TickMark.m
+++ Task/Functions/TickMark.m
@@ -1,4 +1,5 @@
 function TickMark(taskParam, parameter, type)
+%TICKMARK   Draws the tickmark in the cannon task
 
 if isequal(type, 'pred')
     col = [255 165 0];
@@ -16,7 +17,9 @@ end
 rotRad = taskParam.circle.rotationRad + tickNormalization; 
 OutcSpot = parameter - 1;
 zero = taskParam.gParam.zero;
-Screen('FrameArc',taskParam.gParam.window.onScreen, col,[zero(1) - rotRad, zero(2) - rotRad, zero(1) + rotRad, zero(2) + rotRad],OutcSpot, 2, tickLength, [], []) %30
+Screen('FrameArc',taskParam.gParam.window.onScreen, col,...
+    [zero(1) - rotRad, zero(2) - rotRad, zero(1) + rotRad,...
+    zero(2) + rotRad],OutcSpot, 2, tickLength, [], []) 
 
 end
 
--- Task/Functions/VolaIndication.m
+++ Task/Functions/VolaIndication.m
@@ -2,8 +2,10 @@ function VolaIndication(taskParam, txtNoise, txtPressEnter)
 % This function tells the participant whether he is in a high or low
 % volatility environment.
 
-DrawFormattedText(taskParam.gParam.window, txtNoise, 'center','center', [255 255 255]);
-DrawFormattedText(taskParam.gParam.window,txtPressEnter,'center',taskParam.gParam.screensize(4)*0.9);
+DrawFormattedText(taskParam.gParam.window, txtNoise, 'center','center',...
+    [255 255 255]);
+DrawFormattedText(taskParam.gParam.window,txtPressEnter,'center',...
+    taskParam.gParam.screensize(4)*0.9);
 Screen('DrawingFinished', taskParam.gParam.window);
 t = GetSecs;
 Screen('Flip', taskParam.gParam.window, t + 0.1);
