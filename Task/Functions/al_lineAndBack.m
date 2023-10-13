06db3ac0819ff48c6753886f2bb355e19bb69ee6
Rasmus Bruckner <rasmusbruckner@gmail.com>
2023-10-13 21:01:38 +0200

    Cleaned al_lineAndBack function

--- Task/Functions/al_lineAndBack.m
+++ Task/Functions/al_lineAndBack.m
@@ -1,43 +1,42 @@
 function al_lineAndBack(taskParam)
-%AL_LINEANDBACK   This function draws the background for intructions in the cannon task
+%AL_LINEANDBACK This function draws the background for intructions in the cannon task
 %
 %   Input
-%       taskParam: structure containing task parameters
+%       taskParam: Task-parameter-object instance
 %
 %   Output
-%       ~
+%       None
 
 
-if ~isequal(taskParam.gParam.taskType, 'chinese')
-    if isequal(taskParam.colors.background, 'black')
-        col = taskParam.colors.black;
-    else
-        col = taskParam.colors.gray;
-    end
-    Screen('FillRect', taskParam.display.window.onScreen, col);
-    
-    % Present background texture
-    if isequal(taskParam.trialflow.background, 'picture')
-        Screen('DrawTexture', taskParam.display.window.onScreen, taskParam.display.backgroundTxt,[], taskParam.display.backgroundCoords, []);
-    end
-    if isequal(taskParam.colors.background, 'black')
-        col = taskParam.colors.black;
-    else
-        col = taskParam.colors.darkBlue;
-   end
-    Screen('FillRect', taskParam.display.window.onScreen, col, [0, 0, taskParam.display.screensize(3), (taskParam.display.screensize(4)*1/4)]);% -3
-    Screen('FillRect', taskParam.display.window.onScreen, col, [0, taskParam.display.screensize(4)*3/4, taskParam.display.screensize(3), taskParam.display.screensize(4)]);   
-    
-    if ~isequal(taskParam.trialflow.background, 'picture')
-        Screen('DrawLine', taskParam.display.window.onScreen, [0 0 0], 0, taskParam.display.screensize(4)*1/4, taskParam.display.screensize(3), taskParam.display.screensize(4)*1/4, 5);
-        Screen('DrawLine', taskParam.display.window.onScreen, [0 0 0], 0, taskParam.display.screensize(4)*3/4, taskParam.display.screensize(3), taskParam.display.screensize(4)*3/4, 5);
-    end
+% Extract background color
+if isequal(taskParam.colors.background, 'black')
+    col = taskParam.colors.black;
 else
-    Screen('FillRect', taskParam.display.window.onScreen, [66 66 66]);
-    Screen('DrawLine', taskParam.display.window.onScreen, [0 0 0], 0, taskParam.display.screensize(4)*1/4, taskParam.display.screensize(3), taskParam.display.screensize(4)*1/4, 5);
-    Screen('DrawLine', taskParam.display.window.onScreen, [0 0 0], 0, taskParam.display.screensize(4)*6/7, taskParam.display.screensize(3), taskParam.display.screensize(4)*6/7, 5);
-    Screen('FillRect', taskParam.display.window.onScreen, [0 25 51], [0, 0, taskParam.display.screensize(3), (taskParam.display.screensize(4)*1/4)-3]);
-    Screen('FillRect', taskParam.display.window.onScreen, [0 25 51], [0, taskParam.display.screensize(4)*6/7, taskParam.display.screensize(3), taskParam.display.screensize(4)]);
+    col = taskParam.colors.gray;
 end
 
+% Display background
+Screen('FillRect', taskParam.display.window.onScreen, col);
+
+% Present background texture, if desired
+if isequal(taskParam.trialflow.background, 'picture')
+    Screen('DrawTexture', taskParam.display.window.onScreen, taskParam.display.backgroundTxt,[], taskParam.display.backgroundCoords, []);
+end
+
+% Extract background color of lower and upper screen
+if isequal(taskParam.colors.background, 'black')
+    col = taskParam.colors.black;
+else
+    col = taskParam.colors.darkBlue;
+end
+
+% Display background
+Screen('FillRect', taskParam.display.window.onScreen, col, [0, 0, taskParam.display.screensize(3), (taskParam.display.screensize(4)*1/4)]);% -3
+Screen('FillRect', taskParam.display.window.onScreen, col, [0, taskParam.display.screensize(4)*3/4, taskParam.display.screensize(3), taskParam.display.screensize(4)]);
+
+% Display lines to separate different background colors
+if ~isequal(taskParam.trialflow.background, 'picture')
+    Screen('DrawLine', taskParam.display.window.onScreen, [0 0 0], 0, taskParam.display.screensize(4)*1/4, taskParam.display.screensize(3), taskParam.display.screensize(4)*1/4, 5);
+    Screen('DrawLine', taskParam.display.window.onScreen, [0 0 0], 0, taskParam.display.screensize(4)*3/4, taskParam.display.screensize(3), taskParam.display.screensize(4)*3/4, 5);
+end
 
